0e15dd486519f9d09d9a880effd8c84295dd3652
Dawid Bakalarski <dawid.bakalarski@gmail.com>
2018-07-27 13:17:58 +0200

    Try add App.css and sudoku.solve

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,27 +1,67 @@
 import React from 'react';
 import { hot } from 'react-hot-loader';
+import sudoku from 'sudoku-umd';
 
+import styles from './App.css';
 
 class App extends React.Component {
 	constructor(props){
 		super(props);
+
 		this.state = {
-			initialBoard: '',
-			board: ''
-}
+			board: '',
+			initialBoard: ''
+		}
+
+	}
+
+	randBoard(level) {
+		let randomBoard = sudoku.generate(level)
+	    this.setState({
+	     	board: randomBoard,
+			initialBoard: randomBoard
+			})
+	}
+
+	updateNumber(e, index) {
+		let tempBoard = this.state.board.split("").map((item, i) => {
+			if(i == index) return e.target.value;
+			else return item;
+		});
+		this.setState({ board: tempBoard.join("") })
+	}
+	solve(elem) {
+		let sudokuSolve = sudoku.solve(elem)
+		this.setState({
+			board: sudokuSolve
+			//initialBoard: sudokuSolve
+		})
 	}
 
 	render() {
 		return (
-			<div className="App">
-			   <h1>Sudoku</h1>
-			  {/* <Board /> */}
-			   <div className="buttons">
-			       <button>Check</button>
-			       <button>New Game</button>
-			       <button>Solve</button>
-			       <button>Restart</button>
-			   </div>
+			<div>
+				<h1>Sudoku APP</h1>
+
+				<div className={"WelcomeHeader"}>
+           <h2>Witamy w grze</h2>
+					 <button onClick={ () => this.randBoard("easy") }>Easy</button>
+					 <button onClick={ () => this.randBoard("medium") }>Medium</button>
+					 <button onClick={ () => this.randBoard("hard") }>Hard</button>
+					 <button onClick={ () => this.solve(randomBoard) }>Solve</button>
+				</div>
+
+        <div className={"Board"}>
+          { this.state.board.split("").map((item, index) => {
+
+						if(this.state.initialBoard.split("")[index] != ".") return <input key={index} disabled value={item} />
+						else return <input key={index} onChange={(e) => this.updateNumber(e, index)} maxlength="1" /*type="number"*/ value={item} />
+					})
+				}
+				</div>
+
+
+
 			</div>
 		);
 	}
